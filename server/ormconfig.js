e74a6d335d7da382065644b09d761b6ba71bc948
Team Pattarai <77934042+team-pattarai@users.noreply.github.com>
2021-08-22 14:27:46 +0530

    Update ormconfig.js

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,9 +1,9 @@
 const type = process.env.TYPEORM_TYPE || 'postgres';
-const username = process.env.TYPEORM_USERNAME || 'postgres';
-const password = process.env.TYPEORM_PASSWORD || '9592';
-const host = process.env.TYPEORM_HOST || 'localhost';
+const username = process.env.TYPEORM_USERNAME || 'swszlobuxfdcjm';
+const password = process.env.TYPEORM_PASSWORD || '935260b2234ac587aa48deccd73d3435c4648ccae7e8ccb738ec6d220d3ff6cf';
+const host = process.env.TYPEORM_HOST || 'ec2-54-211-160-34.compute-1.amazonaws.com';
 const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
-const database = process.env.TYPEORM_DATABASE || 'bug_tracker';
+const database = process.env.TYPEORM_DATABASE || 'd1o43m22vctijg';
 
 module.exports = {
   type,
